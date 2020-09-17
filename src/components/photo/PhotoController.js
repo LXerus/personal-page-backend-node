47c0aa7058b14b406f39c3d6d822ee8591bd59e5
Cristhian Franco <crodfranco@gmail.com>
2020-09-17 10:24:11 -0600

    preparing for merge

--- src/components/photo/PhotoController.js
+++ src/components/photo/PhotoController.js
@@ -1,30 +1,50 @@
 const PhotoStorage = require("./PhotoStorage");
 const multer = require("multer");
 const path = require("path");
+const host = "localhost";
+const port = 3000;
 
 const storage = multer.diskStorage({
-    destination: "public/images/",
-    filename: (req, file, callback)=>{
-        callback(null, Date.now()+path.extname(file.originalname))
+  destination: "public/images/",
+  filename: (req, file, callback) => {
+    callback(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: (req, file, callback) => {
+    if (!file.originalname.match(/\.(png|jpg|jpeg|jpng)$/)) {
+      return callback(
+        "El archivo tiene que ser una de las siguientes extensiones: png, jpg, jpeg, jpng"
+      );
     }
-  });
-  
-  const upload = multer({
-      storage: storage,
-      fileFilter: (req, file, callback)=>{
-          if(!file.originalname.match(/\.(png|jpg|jpeg|jpng)$/)){
-              return(callback("El archivo tiene que ser una de las siguientes extensiones: png, jpg, jpeg, jpng"));
-          }
-          callback(null, true);
-      }
-  });
+    callback(null, true);
+  },
+});
 
 function addPhoto(title, image) {
-    
+  return new Promise((resolve, reject) => {
+    if (!title || !image) {
+      reject("Invalid file data, image or tittle missing");
+    }
+
+    const newPhoto = {
+      title: title,
+      path: `http:${host}/${port}/${storage.destination}/${image.originalname}`,
+    };
+
+    PhotoStorage.addPhoto(newPhoto);
+    resolve(newPhoto);
+  });
 }
 
-module.exports = {
-    storage,
-    upload,
-    addPhoto,
+function getPhoto(photoName) {
+    
 }
+module.exports = {
+  storage,
+  upload,
+  addPhoto,
+  getPhoto,
+};
