bdaf1788eb1c065c547be5c3f28c02858c8ad13e
Xerus <crodfranco@gmail.com>
2020-10-16 22:21:46 -0600

    security issue fixed

--- src/config/configVars.js
+++ src/config/configVars.js
@@ -1,5 +1,5 @@
 const configVars = {
-    dbUri: process.env.DB_URI || "mongodb+srv://db_standard_user:ZbOjs7EV4zuNeKfN@cluster0.cttdc.mongodb.net/alexa-portfolio",
+    dbUri: process.env.DB_URI,
     port: process.env.PORT || 3000,
     host: process.env.HOST || "0.0.0.0" || "http://localhost",
 }
